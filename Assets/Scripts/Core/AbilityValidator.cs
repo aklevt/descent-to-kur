8069a447d050eb28b8f9e262d3c718b651600b6a
AlexeyKlevtsov <alexey.klevtsov@inbox.ru>
2026-05-06 20:01:09 +0500

    Рефакторинг Ability Controller

---
+++ Assets/Scripts/Core/AbilityValidator.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using Abilities;
+using Entities;
+using Stats;
+using UI;
+using UnityEngine;
+
+namespace Core
+{
+    /// <summary>
+    /// Отвечает за проверку условий для вызова способностей и показ предупреждений в UI
+    /// </summary>
+    public class AbilityValidator
+    {
+        /// <summary>
+        /// Проверяет ресурсы способности и показывает предупреждения в UI
+        /// </summary>
+        public void ValidateAbilityAndWarn(AbilityData ability, int abilityIndex, AbilityBar abilityBar)
+        {
+            if (PlayerMovement.Instance == null) return;
+            
+            var stats = PlayerMovement.Instance.Stats;
+            
+            if (ShouldFlashAbility(ability, stats))
+            {
+                abilityBar?.TriggerWarningFlash(abilityIndex);
+            }
+            
+            ShowAbilityWarnings(ability, stats);
+        }
+
+        /// <summary>
+        /// Проверяет возможность использования способности на указанной цели
+        /// </summary>
+        public bool CanUseAbilityOnTarget(AbilityData ability, Vector3Int targetCell, List<Vector3Int> availableCells)
+        {
+            if (!ability.CanUse(PlayerMovement.Instance))
+            {
+                UIController.Instance?.ShowEnergyWarning();
+                return false;
+            }
+
+            if (!availableCells.Contains(targetCell)) return false;
+
+            if (!ability.IsValidTarget(targetCell, PlayerMovement.Instance))
+                return false;
+                
+            return true;
+        }
+
+        /// <summary>
+        /// Проверяет состояние игрока для выполнения действия
+        /// </summary>
+        public bool IsPlayerReadyForAction()
+        {
+            if (PlayerMovement.Instance.IsFreeze)
+            {
+                UnityEngine.Debug.Log("Игрок заморожен, пропуск хода");
+                return false;
+            }
+            
+            return !PlayerMovement.Instance.IsMoving;
+        }
+
+        private bool ShouldFlashAbility(AbilityData ability, EntityRuntimeStats stats)
+        {
+            if (ability is MoveAbilityData)
+            {
+                return stats.RemainingSteps <= 0 || stats.Energy <= 0;
+            }
+            else
+            {
+                return !stats.HasEnergyForAction(ability.energyCost);
+            }
+        }
+
+        private void ShowAbilityWarnings(AbilityData ability, EntityRuntimeStats stats)
+        {
+            if (ability is MoveAbilityData && stats.RemainingSteps <= 0)
+            {
+                UIController.Instance?.ShowStepsWarning();
+            }
+            
+            if (!ability.CanUse(PlayerMovement.Instance))
+            {
+                UIController.Instance?.ShowEnergyWarning();
+            }
+        }
+    }
+}
